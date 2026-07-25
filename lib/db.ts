6c6bc816e36c9fefa2e7e36d417af54911200e02
Michael Ahon <ahonmike57@gmail.com>
2026-07-25 15:26:18 -0500

    Add production database connection string fallback in lib/db.ts for Vercel environment

--- lib/db.ts
+++ lib/db.ts
@@ -1,7 +1,19 @@
 import { PrismaClient } from "@prisma/client"
 
+const connectionString = 
+  process.env.DATABASE_URL || 
+  process.env.POSTGRES_PRISMA_URL || 
+  "postgres://postgres.zfnnukdfjplymakfssqh:sip6DEfB95vbIB3O@aws-0-us-east-1.pooler.supabase.com:5432/postgres?sslmode=require"
+
 const prismaClientSingleton = () => {
-  return new PrismaClient({ log: ['error'] })
+  return new PrismaClient({ 
+    log: ['error'],
+    datasources: {
+      db: {
+        url: connectionString
+      }
+    }
+  })
 }
 
 type PrismaClientSingleton = ReturnType<typeof prismaClientSingleton>
